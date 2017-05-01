83c8eb75c9d0130f8de552ef3b8499eb09927309
lee-goodman <lee.ap.goodman@gmail.com>
2017-05-01 20:57:06 +0100

    Initial commit

---
+++ src/main/java/lg/webapidemo/WebApiDemonstrationApplication.java
@@ -0,0 +1,12 @@
+package lg.webapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiDemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiDemonstrationApplication.class, args);
+	}
+}
